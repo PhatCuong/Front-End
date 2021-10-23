296f4fb529f4430ab65de8d8757c9aaeb444d955
PhatCuong <cuongphat123bt@gmail.com>
2021-10-23 19:04:37 +0700

    update code

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,152 +1,184 @@
 import React, { useState, useEffect } from "react";
 import { db } from "@/firebase";
 import {
-    Button,
-    Card,
-    Icon,
-    Form,
-    Checkbox,
-    Container,
+  Button,
+  Card,
+  Icon,
+  Form,
+  Checkbox,
+  Container,
+  Table,
+  Label,
+  Image,
+  Grid,
 } from "semantic-ui-react";
 import faker from "faker";
 import "semantic-ui-css/semantic.min.css";
 import "./home.scss";
 
 const getFormattedDate = (date) => {
-    var year = date.getFullYear();
+  var year = date.getFullYear();
 
-    var month = (1 + date.getMonth()).toString();
-    month = month.length > 1 ? month : "0" + month;
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : "0" + month;
 
-    var day = date.getDate().toString();
-    day = day.length > 1 ? day : "0" + day;
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
 
-    return month + "/" + day + "/" + year;
+  return month + "/" + day + "/" + year;
 };
 
 const Home = () => {
-    const [userData, setUserData] = useState([]);
-    const [newData, setNewData] = useState({
-        name: "",
-        dob: "",
-        doses: 0,
-        location: "",
-        phoneNumber: "",
-        address: "",
-    });
+  const [userData, setUserData] = useState({});
+  const fetchData = () => {
+    db.collection("vaccine_passport")
+      .doc("1634463353623")
+      .get()
+      .then((snapshot) => {
+        const document = snapshot.data();
+        // do something with document
+        setUserData(document);
+      });
+  };
 
-    const fetchData = () => {
-        db.collection("vaccine_passport")
-            .get()
-            .then((querySnapshot) => {
-                const data = querySnapshot.docs.map((doc) => ({
-                    ...doc.data(),
-                    id: doc.id,
-                }));
-                for (let i = 0; i < data.length; i++) {
-                    data[i].avt = faker.image.avatar();
-                }
-                setUserData(data); // array of cities objects
-            });
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-    const removeData = (item) => {
-        setUserData((pre) => {
-            return pre.filter((i) => i !== item);
-        });
-    };
-    return (
-        <div className="home">
-            <Container className="home__container">
-                <Form>
-                    <Form.Field>
-                        <label>Location</label>
-                        <input
-                            placeholder="Ho Chi Minh city"
-                            value={newData.description}
-                            onChange={(e) => {
-                                setNewData((pre) => {
-                                    return {
-                                        ...pre,
-                                        description: e.target.value,
-                                    };
-                                });
-                            }}
-                        />
-                    </Form.Field>
-                    <Button type="submit">Submit</Button>
-                </Form>
-            </Container>
-            
-            <div className="home__data">
-                {userData.map((user, index) => (
-                    <Card
-                        key={user.id}
-                        className="home__card"
-                        image={user.avt}
-                        header={user.name}
-                        meta={getFormattedDate(user.dob)}
-                        description={
-                            <div>
-                                <div>Vaccine location: {user.location}</div>
-                                <div>Dose(s): {user.doses}</div>
-                                <div>Phone Number: {user.phoneNumber}</div>
-                                <div>Address: {user.address}</div>
-                            </div>
-                        }    
-                        extra={
-                            <div>
-                                {user.doses == 2 ? (
-                                    <span>
-                                    <Icon
-                                        style={{ color: "green" }}
-                                        name="check circle"
-                                    />
-                                    Fully vaccinated
-                                </span>
-                                ) : (
-                                    user.doses != 1 ? (
-                                        <span>
-                                            <Icon
-                                                style={{ color: "red" }}
-                                                name="cancel"
-                                            />
-                                            Not fully vaccinated
-                                        </span>
-                                        ) : (
-                                            <span>
-                                            <Icon
-                                                style={{ color: "orange" }}
-                                                name="exclamation triangle"
-                                            />
-                                            Not fully vaccinated
-                                        </span>
-                                        )
-                                )}
-                               <div style={{display: "flex", justifyContent: "center", marginTop: "10px"}}>
-                                <Button style = {{paddingRight : "10px", paddingLeft: "10px"}}
-                                    onClick={() => {
-                                        db.collection("vaccine_passport")
-                                            .doc(user.id)
-                                            .delete()
-                                            .then(() => {
-                                                console.log(success);
-                                                fetchData();
-                                            });
-                                    }}>
-                                    <Icon name="user cancel" />
-                                </Button>
-                                </div>
-                            </div>
-                        }
+  return (
+    <div className="home">
+      {/* <-----------------Search by ID-------------------> */}
+      <Container className="home__container">
+        <Form>
+          <Form.Field>
+            <label>Search by ID</label>
+            <input
+              placeholder="0123456789"
+              //   value={newData.description}
+              //   onChange={(e) => {
+              //     setNewData((pre) => {
+              //       return {
+              //         ...pre,
+              //         description: e.target.value,
+              //       };
+              //     });
+              //   }}
+            />
+          </Form.Field>
+          <Button type="Search">Search</Button>
+        </Form>
+      </Container>
+
+      {/* <-----------------Vaccine Status-------------------> */}
+
+      <div className="home__data">
+        <h1
+          className="section__title"
+          style={{ textAlign: "center" }}
+          id="vaccine_status"
+        >
+          Vaccine Status
+        </h1>
+
+        <Image src={faker.image.avatar()} />
+
+        <h2> {userData.name} </h2>
+
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Vaccinated?</Table.HeaderCell>
+              <Table.HeaderCell>Status</Table.HeaderCell>
+              <Table.HeaderCell>Dose(s)</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                {userData.doses == 2 ? (
+                  <span>
+                    <Icon style={{ color: "green" }} name="check circle" />
+                  </span>
+                ) : userData.doses != 1 ? (
+                  <span>
+                    <Icon style={{ color: "red" }} name="cancel" />
+                  </span>
+                ) : (
+                  <span>
+                    <Icon
+                      style={{ color: "orange" }}
+                      name="exclamation triangle"
                     />
-                ))}
-            </div>
-        </div>
-    );
+                  </span>
+                )}
+              </Table.Cell>
+              <Table.Cell>
+                {userData.doses == 2 ? (
+                  <span>
+                    <p> Fully vaccinated</p>
+                  </span>
+                ) : userData.doses != 1 ? (
+                  <span>
+                    <p> Not vaccinated</p>
+                  </span>
+                ) : (
+                  <span>
+                    <p> Not fully vaccinated</p>
+                  </span>
+                )}
+              </Table.Cell>
+              <Table.Cell>{userData.doses}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
+        {/* <-----------------Personal Information-------------------> */}
+
+        <h1 className="section__title" id="info">
+          Personal Information
+        </h1>
+
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <h3>Full name</h3>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <p>{userData.name}</p>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <h3>Date of Birth</h3>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <p>{userData.name}</p>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <h3>Address</h3>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <p>{userData.address}</p>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <h3>Phone Number</h3>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <p>{userData.phoneNumber}</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
